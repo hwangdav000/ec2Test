20d36fa8f06b7af92625986680809f0fe0ff9f86
David Hwang <hwangdav000@gmail.com>
2024-08-28 12:39:17 -0500

    add ping command

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -10,4 +10,9 @@ public class MyController {
 		public String sayHello() {
 			return "API HEALTH good";
 		}
+		
+		@RequestMapping("api/ping")
+		public String sayPing() {
+			return "Server responds good";
+		}
 }
